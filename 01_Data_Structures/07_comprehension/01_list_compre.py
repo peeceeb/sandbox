6881ec6d5feeb7ae47cc54a34be4a4de55d67c63
peeceeb <bagal.prasanna@gmail.com>
2026-09-04 13:08:06 +0530

    Commit as of 4th September 2026

--- 01_Data_Structures/07_comprehension/01_list_compre.py
+++ 01_Data_Structures/07_comprehension/01_list_compre.py
@@ -7,6 +7,7 @@ menu = [
 ]
 
 iced_tea= [tea for tea in menu if "Iced" in tea] 
-iced_tea= [tea1 for tea1 in menu if len(tea1)>12] 
+iced_tea1= [tea1 for tea1 in menu if len(tea1)>12] 
 
 print(iced_tea)
+print(iced_tea1)
