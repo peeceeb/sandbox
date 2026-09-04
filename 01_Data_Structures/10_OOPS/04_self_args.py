5a411053d9425f25712167424fe09d4440769508
peeceeb <bagal.prasanna@gmail.com>
2026-09-04 13:48:00 +0530

    "Edits to self_args"

--- 01_Data_Structures/10_OOPS/04_self_args.py
+++ 01_Data_Structures/10_OOPS/04_self_args.py
@@ -3,12 +3,16 @@ class Chaicup:
 
     def describe(self): #self is the reference to all the properties you are defining in class. 1 or many
         return f"A {self.size} ml chai cup" #you can use self. for a property or a function
+
+    def describe2(self,size): #self is the reference to all the properties you are defining in class. 1 or many
+        self.size=size
+        return f"A {self.size} ml chai cup" #you can use self. for a property or a function
     
 cup=Chaicup()
 print(cup.describe())
-#print(Chaicup.describe())
+print(cup.describe2(50)) #Override size =50
 
 cup2=Chaicup()
 print(cup2.describe())
-cup2.size=100
+cup2.size=100 #Overrise size=100
 print(Chaicup.describe(cup2))
