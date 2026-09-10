b3e41c1e111086b02b1c806b8f6c9b5828904241
peeceeb <bagal.prasanna@gmail.com>
2026-09-10 19:57:22 +0530

    Change configuration to setup in local

--- 06_fastapi/03-products-with-db/database.py
+++ 06_fastapi/03-products-with-db/database.py
@@ -2,6 +2,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:0000@localhost:5432/telusko"
+db_url = "postgresql://postgres:0000@localhost:5432/pcb_todo"
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- 06_fastapi/04-products-with-ui/database.py
+++ 06_fastapi/04-products-with-ui/database.py
@@ -1,7 +1,14 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-db_url = "postgresql://postgres:12345678@localhost:5432/telusko"
+db_url = os.getenv("DATABASE_URL")
+if not db_url:
+	raise RuntimeError(
+		"DATABASE_URL is not set. Set it to your PostgreSQL connection string "
+		"before starting the application."
+	)
+
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
